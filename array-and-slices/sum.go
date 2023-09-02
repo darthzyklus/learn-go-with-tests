ecc91622ec7ac760804e8e2e3543c4fabe01e2ce
Andres Uris <andres.urismontilla@gmail.com>
2023-09-02 19:31:51 +0200

    finish array and slices 2nd part

--- array-and-slices/sum.go
+++ array-and-slices/sum.go
@@ -9,3 +9,28 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbers ...[]int) []int {
+	var sums []int
+
+	for i := range numbers {
+		sums = append(sums, Sum(numbers[i]))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) > 0 {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		} else {
+			sums = append(sums, 0)
+		}
+	}
+
+	return sums
+}

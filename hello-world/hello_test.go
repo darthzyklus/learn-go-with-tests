149ab6ae8bf8097b202b09edf4d860890fa6cf46
Andres Uris <andres.urismontilla@gmail.com>
2025-04-15 23:45:15 -0300

    refactor hello test

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -5,10 +5,25 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("Andres")
-	want := "Hello, Andres"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Andres")
+		want := "Hello, Andres"
+
+		assertEqual(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertEqual(t, got, want)
+	})
+}
+
+func assertEqual[T comparable](t testing.TB, got T, want T) {
+	t.Helper()
 
 	if got != want {
-		t.Errorf("got: %q; want %q", got, want)
+		t.Errorf("got: %v; want %v", got, want)
 	}
 }

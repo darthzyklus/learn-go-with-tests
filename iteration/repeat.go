53ab1cf920b302d2298410b94292a079826890c3
Andres Uris <andres.urismontilla@gmail.com>
2025-04-24 20:03:06 -0300

    add size parameter to repeat function

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -2,10 +2,10 @@ package iteration
 
 import "strings"
 
-func Repeat(character string) string {
+func Repeat(character string, size int) string {
 	var repeated strings.Builder
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < size; i++ {
 		repeated.WriteString(character)
 	}
 

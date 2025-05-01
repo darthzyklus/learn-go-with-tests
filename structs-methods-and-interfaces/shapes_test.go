bf9daa51a374a6f8c8caba4e239f3da84956b77a
Andres Uris <andres.urismontilla@gmail.com>
2025-05-01 18:39:13 +0200

    refactor test area test to improve debug

--- structs-methods-and-interfaces/shapes_test.go
+++ structs-methods-and-interfaces/shapes_test.go
@@ -15,22 +15,25 @@ func TestPerimeter(t *testing.T) {
 }
 
 type AreaTestCase struct {
+	name  string
 	shape Shape
 	want  float64
 }
 
 func TestArea(t *testing.T) {
 	areaTests := []AreaTestCase{
-		{shape: Rectangle{width: 12.0, height: 6.0}, want: 72.0},
-		{shape: Circle{radius: 10.0}, want: math.Pi * 100},
-		{shape: Triangle{base: 12, height: 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{width: 12.0, height: 6.0}, want: 72.0},
+		{name: "Circle", shape: Circle{radius: 10.0}, want: math.Pi * 100},
+		{name: "Triangle", shape: Triangle{base: 12, height: 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
 
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
 	}
 }

7d2a6f0bf99f15938cf9b4dbf443f81e45f8524f
Andres Uris <andres.urismontilla@gmail.com>
2025-05-04 23:55:05 +0200

    add test case to evaluate multiple deposits in the wallet

--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -3,14 +3,29 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	t.Run("first deposit should be get the same amount of balance", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(10)
+		wallet.Deposit(10)
 
-	got := wallet.Balance()
-	want := 10
+		got := wallet.Balance()
+		want := 10
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+	t.Run("multiple deposits should get a balance equal to the sum of the deposit amounts", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(10)
+		wallet.Deposit(10)
+
+		got := wallet.Balance()
+		want := 20
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 }

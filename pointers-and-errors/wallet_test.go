27d99941db368ccfe68405d77c51a765a266c059
Andres Uris <andres.urismontilla@gmail.com>
2025-05-07 23:37:45 +0200

    implement withdraw method for wallet struct

--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	t.Run("first deposit should be get the same amount of balance", func(t *testing.T) {
+	t.Run("single deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
@@ -15,7 +15,7 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %s want %s", got, want)
 		}
 	})
-	t.Run("multiple deposits should get a balance equal to the sum of the deposit amounts", func(t *testing.T) {
+	t.Run("multiple deposits", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
@@ -23,6 +23,20 @@ func TestWallet(t *testing.T) {
 
 		got := wallet.Balance()
 		want := Bitcoin(20)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{
+			balance: Bitcoin(20),
+		}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)

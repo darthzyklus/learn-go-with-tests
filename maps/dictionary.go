fb1a1db7edb1ce25912ac9a0ee24bb04ca2bca43
Andres Uris <andres.urismontilla@gmail.com>
2025-05-22 00:02:47 +0200

    refactor definition validation on dictionary search

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -7,9 +7,9 @@ type Dictionary map[string]string
 func (d Dictionary) Search(word string) (string, error) {
 	value, ok := d[word]
 
-	if ok {
-		return value, nil
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
 	}
 
-	return "", errors.New("could not find the word you were looking for")
+	return value, nil
 }

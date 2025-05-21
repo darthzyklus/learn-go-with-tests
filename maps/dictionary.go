16f0d598fe7b496c0b8a853d5c98100419bf41ab
Andres Uris <andres.urismontilla@gmail.com>
2025-05-21 23:59:13 +0200

    add unknown word test case

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,15 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	value := d[word]
+
+	if value == "" {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return value, nil
 }
